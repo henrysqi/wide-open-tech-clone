9bec6bf85a4ddd39491bdfe02f580479c2c94529
Henry Qi <henryqi222@gmail.com>
2016-10-13 14:57:08 -0600

    pretty much done. few alignment bugs and logo

--- script.js
+++ script.js
@@ -8,11 +8,25 @@ $(document).ready(function(){
           $('#nav').css('background-color', 'white');
 					$('#nav').css('z-index', '10');
 					$('#nav ul li').css('color', '#6A787C');
-					$('#nav img').attr('src', 'imgs/dark.jpg');
+					$('#nav .logo').attr('display', 'none');
+					$('#nav .dark').attr('display', 'block');
        } else {
           $('#nav').css('background-color', 'transparent');
-					$('#nav img').attr('src', 'imgs/title_wideopentech.png');
+					$('#nav .logo').attr('display', 'block');
+					$('#nav .dark').attr('display', 'none');
 					$('#nav ul li').css('color', 'white');
        }
    });
+	
+	$(document).scroll(function() {
+		scroll_start = $(this).scrollTop();
+		if (scroll_start > offset.top) {
+			$('.dark').removeClass('hidden');
+			$('.logo').addClass('hidden');
+		}
+		else {
+			$('.dark').addClass('hidden');
+			$('.logo').removeClass('hidden');
+		}
+	})
 });
